08cb16d81196ceaa3dac853c65c980b4dabcca00
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 16:10:31 +0000

    Adding numeric_owner as keyword arguement

--- checkbox_ng/launcher/merge_submissions.py
+++ checkbox_ng/launcher/merge_submissions.py
@@ -77,7 +77,7 @@ class MergeSubmissions(MergeReports):
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, tmpdir.name)
